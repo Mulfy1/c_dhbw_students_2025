3783f7034f04235b510bb03a11e0bf1178ef21e8
Mulfy1 <9cjry4vq9r@privaterelay.appleid.com>
2026-01-12 16:02:27 +0100

    aufagben

--- 0010_DYNAMIC_MEMORY_MANAGEMENT/Exercise_dynamic_array/main.c
+++ 0010_DYNAMIC_MEMORY_MANAGEMENT/Exercise_dynamic_array/main.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() 
+int main(void)
 {
-
-
+    size_t length = 0;
+    int sum = 0;
+    printf("Enter the number of elements: ");
+    scanf("%zu", &length);
+    if (length <= 0)
+    {
+        printf("\nInvalid input! The number of elements must be greater than zero.");
+        return 0;
+    }
+    int *heapArray = (int *)malloc(sizeof(int) * length);
+    if (heapArray == NULL)
+    {
+        printf("\nMemory allocation failed.");
+    }
+    printf("\nEnter %zu integers:", length);
+    ;
+    for (size_t i = 0; i < length; i++)
+    {
+        scanf("%d", &heapArray[i]);
+        sum += heapArray[i];
+    }
+    printf("\nThe sum of the array elements is: %d\n", sum);
+    free(heapArray);
+    heapArray = NULL;
     return 0;
 }
--- 0010_DYNAMIC_MEMORY_MANAGEMENT/intro/main.c
+++ 0010_DYNAMIC_MEMORY_MANAGEMENT/intro/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
+    size_t length = 3;
+    size_t memoryReq = length * sizeof(int);
+
+    printf("\nadresse of length: %p", (void *)&length);
+
+    printf("\nsizeof(int): %lu", sizeof(int));
+    printf("\nmemoryReq: %zu", memoryReq);
+
+    int *heapArray = (int *)malloc(memoryReq);
+
+    for (size_t i = 0; i < length; i++)
+    {
+        heapArray[i] = (int)i * 100;
+    }
+    for (size_t i = 0; i < length; i++)
+    {
+        printf("\nheapArray[%ld]: %d", i, heapArray[i]);
+        printf("\nadress of heapArray[%ld]: %p\n", i, (void *)&heapArray);
+    }
+
+    free(heapArray);
+    heapArray = (void *)0; /* == NULL */ // sonst dangling pointer
 
     return 0;
 }
